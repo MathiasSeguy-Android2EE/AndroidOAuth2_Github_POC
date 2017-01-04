c212254ccaee0b6a33091b39ba65d542b7df6053
MathiasSeguy-Android2EE <mathias.seguy@android2ee.com>
2017-01-04 14:23:56 +0100

    Initialize project

---
+++ app/src/main/java/com/renaultnissan/acms/platform/oauth/githubsample/MainActivity.java
@@ -0,0 +1,13 @@
+package com.renaultnissan.acms.platform.oauth.githubsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
